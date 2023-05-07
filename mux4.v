af409033469cc7072750c09937248512c3d2d0ff
seon deok Kim <76850241+du6293@users.noreply.github.com>
2023-05-08 03:59:27 +0900

    Create mux4.v

---
+++ mux4.v
@@ -0,0 +1,17 @@
+module mux4 (
+
+	input			    [3:0]		mux_in_a ,
+	input			    [3:0] 	mux_in_b ,
+	input				 		      mux_sel  ,
+	output reg	  [3:0]		mux_out   
+  
+);
+
+	always @(*) //begin
+		if (mux_sel) 
+			mux_out = mux_in_b;
+		else 
+			mux_out = mux_in_a;
+	//end
+	
+endmodule
